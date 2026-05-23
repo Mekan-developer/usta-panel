075953ab132ffadee2a54d2eceb071868c942756
Mekan <mekan.developer@gmail.com>
2026-05-23 13:11:21 +0500

    fix: bootstrap/app.php uluchshil dobawil ->trustProxies(at: '*');

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,13 +15,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             SetLocale::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
